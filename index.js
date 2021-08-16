30eb43efd21bc3ce937141d88364bf5cde387933
FahadCowboy <olivierafahad@gmail.com>
2021-08-16 15:22:41 +0600

    name is changed

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ document.querySelector('#submit-btn').addEventListener('click', function(){
     const userEmail = document.querySelector('#email-field').value
     const userPassword = document.querySelector('#password-field').value
     
-    if(userEmail === 'olivierafahad' && userPassword === '.oliviera.'){
-        window.location.href = 'index.html'
+    if(userEmail === 'email@gmail.com' && userPassword === 'password'){
+        window.location.href = 'banking.html'
     }
 })
 
